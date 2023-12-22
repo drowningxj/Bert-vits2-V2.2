10863dcba6bf3772c9143bf83e5f7f233922c38d
Zheng <zhengma8@gmail.com>
2023-12-22 22:04:42 +0800

    Update short_audio_transcribe.py

--- short_audio_transcribe.py
+++ short_audio_transcribe.py
@@ -51,8 +51,8 @@ if __name__ == "__main__":
         }
     assert (torch.cuda.is_available()), "Please enable GPU in order to run Whisper!"
     model = whisper.load_model(args.whisper_size)
-    #parent_dir = "./custom_character_voice/"
-    parent_dir=config.resample_config.in_dir
+    parent_dir = "./custom_character_voice/"
+    #parent_dir=config.resample_config.in_dir
     print(parent_dir)
     speaker_names = list(os.walk(parent_dir))[0][1]
     speaker_annos = []
